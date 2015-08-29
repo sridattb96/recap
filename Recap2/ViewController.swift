2eafceb82002e62beaf65a8b3abc898acd76697e
Sridatt Bhamidipati <sbhamidi@ucdavis.edu>
2015-08-29 03:01:31 -0700

    Initial Commit

---
+++ Recap2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recap2
+//
+//  Created by Sridatt Bhamidipati on 8/29/15.
+//  Copyright © 2015 SRI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
